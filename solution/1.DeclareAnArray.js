1c2848af444b59b4d3a9c7b8053fbfa0f0fa7311
tanvir243 <travelertanvir@gmail.com>
2024-01-31 01:51:42 +0600

    task-1 completed

---
+++ solution/1.DeclareAnArray.js
@@ -0,0 +1,9 @@
+/***
+ * Task-1
+ * Declare an array
+ */
+let fruits = ["Apple", "Banana", "Blackberrie", "Blueberrie", "Mango"];
+console.log(fruits[3]);
+
+fruits[2] = "jambura";
+console.log(fruits);
