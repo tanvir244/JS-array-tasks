1245d0a6915e9feca52afa5acf8d8c1bfd7aa871
tanvir243 <travelertanvir@gmail.com>
2024-01-31 07:36:09 +0600

    task-2 completed

---
+++ solution/2.AddRemoveElements.js
@@ -0,0 +1,13 @@
+/***
+ * Task-2
+ * Add or remove elements
+ */
+const tourist = ["Cox's Bazer", "Sajek", "Rangamati"]
+tourist.push("Bandarban");
+console.log(tourist);
+
+tourist.push("B-Bariya", "Panam City");
+console.log(tourist);
+
+tourist.pop();
+console.log(tourist);
