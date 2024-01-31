56ac8c5ec59d556bec73a83cbc4fdd97977524fd
tanvir243 <travelertanvir@gmail.com>
2024-01-31 07:52:27 +0600

    task-3 completed

---
+++ solution/3.Checking-Array-Membership-with-Includes.js
@@ -0,0 +1,11 @@
+/***
+ * Task-3
+ * Checking Array Membership with ‘includes’
+ */
+const books = ["Dopamin Detox", "Learn JavaScript", "Stop Over Thinking"]
+if(books.includes("Learn JavaScript")){
+    console.log("Yes, the book available in library");
+}
+else{
+    console.log("No, the book left for now");
+}
