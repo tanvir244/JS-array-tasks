d18ecdeb9bcf9462384bd085d334cbd7c7dce0ec
tanvir243 <travelertanvir@gmail.com>
2024-01-31 08:12:41 +0600

    task-5 completed

---
+++ solution/5.Combining-Arrays.js
@@ -0,0 +1,12 @@
+/***
+ * Task-5
+ * Combining Arrays
+ */
+const country = ["Switzerland", "Germany", "France"];
+const city = ["Zurich", "Berlin", "Paris"];
+
+console.log(country);
+console.log(city);
+
+const combinedArry = country.concat(city);
+console.log(combinedArry);
